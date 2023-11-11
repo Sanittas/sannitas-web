e7c0bc855b60cfab419acd8cc52dfa874734fd63
alanaquinoslv <alan.silva@sptech.school>
2023-11-11 01:03:13 -0300

    update file settings

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Logoff from './pos-login/Logoff';
 import Cadastro from './pages/Cadastro';
 import CadastroEmpresa from './pages/CadastroEmpresa';
 import RedefinirSenha from './pages/redefinirSenha';
-import TrocaSenha from './pages/trocaSenha'; 
+import TrocaSenha from './pages/trocaSenha';
 import Cliente from './pages/cliente';
+import Dashboard from './pages/Dashboard';
 
 
 
@@ -19,11 +20,12 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/redefinirSenha" element={<RedefinirSenha />} />
         <Route path='/login/logoff' element={<Logoff />} />
-        <Route path="/cadastro/*" element={<Cadastro/>} />
-        <Route path="/cadastroEmpresa" element={<CadastroEmpresa/>} />
-        <Route path="/validarToken/:token" element={<TrocaSenha/>} />
-        <Route path="/cliente/:idUsuario" element={<Cliente/>} />
-        <Route path="/cliente/:idUsuario" element={<Cliente/>} />
+        <Route path="/cadastro/*" element={<Cadastro />} />
+        <Route path="/cadastroEmpresa" element={<CadastroEmpresa />} />
+        <Route path="/validarToken/:token" element={<TrocaSenha />} />
+        <Route path="/cliente/:idUsuario" element={<Cliente />} />
+        <Route path="/cliente/:idUsuario" element={<Cliente />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
