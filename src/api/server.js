c8087f954b779531397abf9a61e3af80d19d2c28
alanaquinoslv <alan.silva@sptech.school>
2023-11-29 17:41:08 -0300

    update

--- src/api/server.js
+++ src/api/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '--',
+    password: 'root',
     database: 'sanittas',
 });
 db.connect();
