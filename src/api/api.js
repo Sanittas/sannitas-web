9c68f4f72f3ca1ff73200fc5a35e3c15aac519a8
gabrielfernando23 <gabriel.ssilva@sptech.school>
2024-03-19 11:04:26 -0300

    aa

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8080/"});
+const api = axios.create({ baseURL: "http://10.0.0.167:8080/"});
 
 //Servico-Empresa
-const api8080 = axios.create({ baseURL: "http://localhost:8080/"});
+const api8080 = axios.create({ baseURL: "http://10.0.0.167:8080/"});
 
 //Usuario
-const api8081 = axios.create({ baseURL: "http://localhost:8081/"});
+const api8081 = axios.create({ baseURL: "http://10.0.0.167:8081/"});
 
 
 export { api, api8080, api8081};
